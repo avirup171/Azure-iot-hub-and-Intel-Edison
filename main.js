ad53907caabc23dfe1f5ea242e7500bf36b401b3
Avirup Basu <ami.avirup17@gmail.com>
2017-05-28 18:12:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-/*
- * Blank IoT Node.js starter app.
- *
- * Use this template to start an IoT Node.js app on any supported IoT board.
- * The target board must support Node.js. It is helpful if the board includes
- * support for I/O access via the MRAA and UPM libraries.
- *
- * <https://software.intel.com/en-us/xdk/docs/using-templates-nodejs-iot>
- */
-
-/* spec jslint and jshint lines for desired JavaScript linting */
-/* see http://www.jslint.com/help.html and http://jshint.com/docs */
-/* jslint node:true */
-/* jshint unused:true */
-
 "use strict" ;
 
 
@@ -23,7 +8,7 @@ var Client = require('azure-iot-device').Client;
 var Message = require('azure-iot-device').Message;
 
 //
-//
+//Azure methods
 //
 var connectionString="HostName=mcu.azure-devices.net;DeviceId=edisonAvirup;SharedAccessKey=LXyvqh0UMIwnBsYIpbkOF4NhTO6wyGveSy/NcFy4pig=";
 var client = Client.fromConnectionString(connectionString, Protocol);
